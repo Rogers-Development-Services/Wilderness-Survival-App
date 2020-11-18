335063cb36b6c2ec3523c7a2c013133f7de1b9a3
Jeneth Diesta <jen6one9@gmail.com>
2020-11-17 21:28:44 -0800

    home page template

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        [AUTH O PLACE HOLDER]
+      </p>
+    </div>
+  );
+}
+
+export default Home;

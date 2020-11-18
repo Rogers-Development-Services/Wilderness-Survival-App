7f5fbab8783f5a4ac5e9ab81ee44df07d6289199
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2020-11-17 21:05:42 -0800

    installing react-materialize package in client dir.

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import 'materialize-css';
+import { Button, Card, Row, Col } from 'react-materialize';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();

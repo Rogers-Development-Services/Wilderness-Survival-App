383e21297bc4e1f75febc90471655b215e04575a
Monk <tbsanders5@gmail.com>
2020-11-19 19:44:41 -0800

    got authentication working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const loginButton = () => {
+const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
-    return <button onClick={() => loginWithRedirect()}>Log In</button>
+    return (<button onClick={() => loginWithRedirect()}>Log In</button>)
 };
 
-export default loginButton;
+export default LoginButton;
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,14 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import 'materialize-css';
 import { Button, Card, Row, Col } from 'react-materialize';
+import {Auth0Provider} from '@auth0/auth0-react';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<Auth0Provider
+    domain="dev-qajxs-8o.us.auth0.com"
+    clientId="HxkBw2D995h4Okr9JDCjo3uAEEz8BdD0"
+    redirectUri={window.location.origin}>
+<App />
+</Auth0Provider>, 
+document.getElementById("root"));
 registerServiceWorker();

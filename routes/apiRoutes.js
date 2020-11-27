ad4713bc66d64133e1259c2d637c3303fd76b024
Elliott Jones <jones.elliott@outlook.com>
2020-11-27 15:12:37 -0800

    Created route to update notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
     db.Tip.find({})
       .then(dbTip => {
         console.log(dbTip);
-        res.json(dbTip);
+        res.json(dbTip).code(302);
       })
       .catch(error => {
         console.log(message);
@@ -35,13 +35,14 @@ module.exports = function (app) {
   // Routes for user notes
 
   // Creates a new user in the db
-  app.post("/notes", ({ body }, res) => {
+  app.post("/create/notes", ({ body }, res) => {
     const note = body;
     console.log(body);
 
     db.Note.create({ userID: note.userID, title: note.title, body: note.body })
       .then(dbNote => {
         console.log(dbNote);
+        res.json(dbNote).code(201);
       })
       .catch(({ message }) => {
         console.log(message);
@@ -53,25 +54,27 @@ module.exports = function (app) {
     db.Note.find({ userID: req.params })
       .then(dbNote => {
         console.log(dbNote);
-        res.json(dbNote);
+        res.json(dbNote).code(302);
       })
       .catch(error => {
         console.log(message);
-        res.json(error)
+        res.json(error).code
       });
   });
 
   // Update specified note
-  app.post("/notes", ({ body }, res) => {
+  app.post("/update/notes", ({ body }, res) => {
     const note = body;
     console.log(body);
 
-    db.Note.updateOne({ id: note.id, userID: note.userID })
+    db.Note.updateOne({ id: note.id, userID: note.userID }, { title: note.title, body: note.body })
       .then(dbNote => {
         console.log(dbNote);
+        res.json(dbNote).code(202);
       })
       .catch(({ message }) => {
         console.log(message);
+        res.json(message).code(304);
       });
   });
 

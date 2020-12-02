2828eee99fa5536df70b1256cc4fdc625ecbab7e
Monk <tbsanders5@gmail.com>
2020-12-01 20:02:58 -0800

    installed new packages, working homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Login from "../components/Login"
 import LoginButton from "../components/Login";
-import Auth0Client from "../components/Offline";
+import auth0Client from "../components/Offline"
 
 function Home() {
   return (
     <div>
-      <h1>Home Page <span id="username"></span></h1>
-      <button type="submit" id="btn-login">Sign In</button>
-      <button type="submit" id="btn-logout">Sign Out</button>
+      <h1>Nomad</h1>
+      <div className="background-position: center center">
+      <Login />
+      </div>
     </div>
   );
 }

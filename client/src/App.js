b94b396df055c0818fddf54e90140966d0c8aeb2
Elliott Jones <jones.elliott@outlook.com>
2020-12-08 18:58:00 -0800

    Auth0 Stuff

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ function App() {
   return (
     <Auth0Provider
       domain="dev-qajxs-8o.us.auth0.com"
-      clientId="HxkBw2D995h4Okr9JDCjo3uAEEz8BdD0"
+      clientId="YG74ZPNcrWeh30VknRhK74NFfF6qvnDm"
       redirectUri={window.location.origin}
     >
       <Router>

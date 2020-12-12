fb519f2e40cbed97f2f89460007c334dde3f63f6
Elliott Jones <jones.elliott@outlook.com>
2020-12-12 14:47:43 -0800

    Refactored update route for notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,107 +1,104 @@
 const db = require("../models");
 const app = require("express").Router()
 
-  // Retrieve all tips in db
-  app.get("/tips", (req, res) => {
-    db.Tip.find({})
-      .then(dbTip => {
-        res.json(dbTip);
-      })
-      .catch(error => {
-        console.log(error);
-        res.json(error)
-      });
-  });
-  
-  // Retrieve all plants in db
-  app.get("/plants", (req, res) => {
-    db.Plant.find({})
-      .then(dbPlant => {
-        res.json(dbPlant);
-      })
-      .catch(error => {
-        console.log(error);
-        res.json(error)
-      });
-  });
- 
-  // Retrieve all animals in db
-  app.get("/animals", (req, res) => {
-    db.Animal.find({})
-      .then(dbAnimals => {
-        res.json(dbAnimals);
-      })
-      .catch(error => {
-        console.log(error);
-        res.json(error)
-      });
-  });
+// Retrieve all tips in db
+app.get("/tips", (req, res) => {
+  db.Tip.find({})
+    .then(dbTip => {
+      res.json(dbTip);
+    })
+    .catch(error => {
+      console.log(error);
+      res.json(error)
+    });
+});
 
-  // Retrieve all checklist info from db
-  app.get("/checklist", (req, res) => {
-    db.Checklist.find({})
-      .then(dbChecklist => {
-        console.log(dbChecklist);
-        res.json(dbChecklist);
-      })
-      .catch(error => {
-        console.log(error);
-        res.json(error)
-      });
-  });
+// Retrieve all plants in db
+app.get("/plants", (req, res) => {
+  db.Plant.find({})
+    .then(dbPlant => {
+      res.json(dbPlant);
+    })
+    .catch(error => {
+      console.log(error);
+      res.json(error)
+    });
+});
 
-  // Creates a new user in the db
-  app.post("/api/create/notes", ({ body }, res) => {
-    const note = body;
-    console.log(note);
-    console.log(body, "create route");
+// Retrieve all animals in db
+app.get("/animals", (req, res) => {
+  db.Animal.find({})
+    .then(dbAnimals => {
+      res.json(dbAnimals);
+    })
+    .catch(error => {
+      console.log(error);
+      res.json(error)
+    });
+});
 
-    db.Note.create({ userID: note.userID, title: note.title, note: note.note })
-      .then(dbNote => {
-        res.json(dbNote);
-      })
-      .catch(({ message }) => {
-        console.log(message);
-      });
-  });
+// Retrieve all checklist info from db
+app.get("/checklist", (req, res) => {
+  db.Checklist.find({})
+    .then(dbChecklist => {
+      console.log(dbChecklist);
+      res.json(dbChecklist);
+    })
+    .catch(error => {
+      console.log(error);
+      res.json(error)
+    });
+});
 
-  // Retrieve all notes for specified user
-  app.get("/api/notes", (req, res) => {
-    db.Note.find({ userID: req.query.id })
-      .then(dbNote => {
-        res.json(dbNote);
-      })
-      .catch(error => {
-        console.log(error);
-        res.json(error);
-      });
-  });
+// Creates a new user in the db
+app.post("/api/notes", ({ body }, res) => {
+  const note = body;
+  console.log(note);
+  console.log(body, "create route");
 
-  // Delete note with specified id
-  app.delete("/api/notes", (req, res) => {
-    db.Note.findOneAndDelete({ _id: req.body })
-      .then(dbNote => {
-        res.json(dbNote);
-      })
-      .catch(error => {
-        console.log(error);
-        res.json(error);
-      });
-  });
+  db.Note.create({ userID: note.userID, title: note.title, note: note.note })
+    .then(dbNote => {
+      res.json(dbNote);
+    })
+    .catch(({ message }) => {
+      console.log(message);
+    });
+});
 
-  // Update specified note
-  app.put("/api/update/notes", ({ body }, res) => {
-    const note = body;
-    console.log(body);
+// Retrieve all notes for specified user
+app.get("/api/notes", (req, res) => {
+  db.Note.find({ userID: req.query.id })
+    .then(dbNote => {
+      res.json(dbNote);
+    })
+    .catch(error => {
+      console.log(error);
+      res.json(error);
+    });
+});
 
-    db.Note.updateOne({ id: note.id, userID: note.userID }, { title: note.title, note: note.note })
-      .then(dbNote => {
-        res.json(dbNote);
-      })
-      .catch(({ message }) => {
-        console.log(message);
-        res.json(message);
-      });
-  });
+// Delete note with specified id
+app.delete("/api/notes", (req, res) => {
+  db.Note.findOneAndDelete({ _id: req.body })
+    .then(dbNote => {
+      res.json(dbNote);
+    })
+    .catch(error => {
+      console.log(error);
+      res.json(error);
+    });
+});
 
-  module.exports = app
+// Update specified note
+app.put("/api/notes", ({ body }, res) => {
+  const note = body;
+  console.log(body);
+
+  db.Note.findOneAndUpdate({ _id: body._id }, { title: body.title, note: body.note }, {new: true}, function(error, document) {
+    if (error) throw error;
+    res.json(document);
+    console.log(document);
+  })
+});
+
+module.exports = app
